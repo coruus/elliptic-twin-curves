bb47755e5e5a85a09f080d05386dd791b6f07ea2
David Leon Gil <coruus@gmail.com>
2015-06-09 09:57:14 -0700

    A bit of cleanup.

--- count.sh
+++ count.sh
@@ -2,3 +2,4 @@ printf "sol224:    %6u    %6u\n" $(ag -l "pE: 1" sol224 | wc -l | tr -d ' \n') $
 printf "sol256:    %6u    %6u\n" $(ag -l "\[1, [01]\]" sol256 | wc -l | tr -d ' \n') $(ag -l "\[1, 1\]" sol256 | wc -l | tr -d ' \n')
 #printf "M31   :    %6u    %6u\n" $(ag -l "pE: 1" sol31 | wc -l | tr -d ' \n') $(ag -l "p2: 1" sol31 | wc -l | tr -d ' \n')
 printf "sol384:    %6u    %6u\n" $(ag -l "pE: 1" sol384 | wc -l | tr -d ' \n') $(ag -l "p2: 1" sol384 | wc -l | tr -d ' \n')
+printf "sol256a:    %6u    %6u\n" $(ag -l "pE: 1" sol256a | wc -l | tr -d ' \n') $(ag -l "p2: 1" sol256a | wc -l | tr -d ' \n')
